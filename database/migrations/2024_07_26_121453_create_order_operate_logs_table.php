dbeb1bd0454e329bb42fe5c7f1820e73b00a9b35
H_ <13105592748@163.com>
2024-07-26 21:02:07 +0800

    update

---
+++ database/migrations/2024_07_26_121453_create_order_operate_logs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderOperateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_operate_logs', function (Blueprint $table) {
+            $table->id();
+            $table->integer('order_id')->comment('订单id');
+            $table->tinyInteger('type')->comment('事件类型1发邮件 2节点确认');
+            $table->text('content·')->nullable()->comment('关键数据');
+            $table->string('operator')->comment('操作人');
+            $table->dateTime('operate_at')->comment('操作时间');
+            $table->timestamps();
+        });
+        $prefix = DB::getConfig('prefix');
+        DB::statement("ALTER TABLE `{$prefix}order_operate_logs` comment '操作记录表'");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_operate_logs');
+    }
+}
