cf4c3440a75385244af0c4fcb61ee39ecabdaaed
server <test@server.com>
2024-07-29 16:55:28 +0800

    1

--- app/Mail/MailCustom.php
+++ app/Mail/MailCustom.php
@@ -30,7 +30,7 @@ class MailCustom extends Mailable
         $this->content = $content;
         $this->from = $from;
         $this->name = $name;
-        $this->files = $files;
+        $this->files = array_map([\App\Utils\Order\OrderFiles::getInstance(), 'tryToFilePath'], $files);
     }
 
     /**
@@ -42,7 +42,7 @@ class MailCustom extends Mailable
     {
         $files = is_array($this->files) ? $this->files : implode(',', $this->files);
         foreach ($files as $file){
-            $this->attach(formatFile($file));
+            $this->attach($file);
         }
         return $this->text('emails.custom')
             ->from($this->from, $this->name)
